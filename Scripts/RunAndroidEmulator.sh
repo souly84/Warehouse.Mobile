024af9ee6d9dd326dcd8812d7093f94c7e367a48
Denis Zhukovski <dzhukovsky@softek.by>
2021-08-22 09:23:30 +0300

    start emulator script update

--- Scripts/RunAndroidEmulator.sh
+++ Scripts/RunAndroidEmulator.sh
@@ -21,7 +21,7 @@ else
     echo "Starting emulator"
 
     # Start emulator in background
-    nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot > /dev/null 2>&1 &
+    nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot -no-boot-anim -gpu auto -qemu > /dev/null 2>&1 &
     $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
 
     $ANDROID_HOME/platform-tools/adb devices
