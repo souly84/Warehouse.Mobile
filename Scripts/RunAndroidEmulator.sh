6c063a253a4c2fa36c0229082d1879934fe42888
Denis Zhukovski <dzhukovsky@softek.by>
2021-08-24 08:04:39 +0300

    more tests

--- Scripts/RunAndroidEmulator.sh
+++ Scripts/RunAndroidEmulator.sh
@@ -10,22 +10,32 @@ if [[ $DEVICES == *"emulator-"* ]]; then
 }
 else
 {
+  
     # Install AVD files
     echo "y" | $ANDROID_HOME/tools/bin/sdkmanager --install 'system-images;android-28;google_apis;x86'
 
+
+
     # Create emulator
-    echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis;x86' --force
+    echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n xamarin_android_emulator -k 'system-images;android-28;google_apis;x86' -d 17 --force
+    
+    echo "Emulator created successfully $(ANDROID_HOME/emulator/emulator -list-avds), launching it"
+    #echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n #xamarin_android_emulator -k #'system-images;android-28;google_apis;x86' --force
 
     $ANDROID_HOME/emulator/emulator -list-avds
 
     echo "Starting emulator"
     
-    # Start emulator in background
-    nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot -no-boot-anim -gpu auto -qemu > /dev/null 2>&1 &
-    $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+    nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -skin 1080x1920 -no-snapshot -no-audio -no-boot-anim -accel auto -gpu auto -qemu -lcd-density 420 > /dev/null 2>&1 &
+    $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done'
+    $ANDROID_HOME/platform-tools/adb devices echo "Emulator started"
     
-    $ANDROID_HOME/platform-tools/adb devices
+    # Start emulator in background
+    #nohup $ANDROID_HOME/emulator/emulator -avd xamarin_android_emulator -no-snapshot -no-boot-anim -gpu auto -qemu > /dev/null 2>&1 &
+    #$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
+    
+    #$ANDROID_HOME/platform-tools/adb devices
 
-    echo "Emulator started"
+    #echo "Emulator started"
 }
 fi
