6552f8c8a31605781e037b8b7edd21ae072c1616
Denis Zhukovski <dzhukovsky@softek.by>
2021-12-25 21:49:54 +0300

    packages updates

--- tests/Warehouse.Mobile.UnitTests/MenuSelectionViewModelTests.cs
+++ tests/Warehouse.Mobile.UnitTests/MenuSelectionViewModelTests.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Warehouse.Mobile.ViewModels;
+using Xunit;
+
 namespace Warehouse.Mobile.UnitTests
 {
     public class MenuSelectionViewModelTests
     {
+        private App _app;
+
         public MenuSelectionViewModelTests()
         {
+            Xamarin.Forms.Mocks.MockForms.Init();
+            _app = new App(new MockPlatformInitializer());
+        }
+
+        [Fact]
+        public void SuppliersNavigation()
+        {
+            var viewModel = new MenuSelectionViewModel(
+                _app.Navigation
+            );
+            viewModel.GoToAvailableSuppliersCommand.Execute(null);
+            //Assert.NotNull(
+               
+            //);
         }
     }
 }
