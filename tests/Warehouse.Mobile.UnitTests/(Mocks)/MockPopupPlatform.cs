42295c8a740d3568c65ecd2ff21f177cc75638ab
Denis Zhukovski <dzhukovsky@softek.by>
2022-01-18 20:15:26 +0300

    fix file not found exception for unit tests

---
+++ tests/Warehouse.Mobile.UnitTests/(Mocks)/MockPopupPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Rg.Plugins.Popup.Contracts;
+using Rg.Plugins.Popup.Pages;
+using Warehouse.Mobile.UnitTests.Mocks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(MockPopupPlatform))]
+
+namespace Warehouse.Mobile.UnitTests.Mocks
+{
+    public class MockPopupPlatform : IPopupPlatform
+    {
+        public bool IsInitialized => true;
+
+        public bool IsSystemAnimationEnabled => false;
+
+        public event EventHandler OnInitialized;
+
+        public List<PopupPage> ShownPopups { get; private set; }
+
+        public List<PopupPage> VisiblePopup { get; private set; }
+
+        public Task AddAsync(PopupPage page)
+        {
+            ShownPopups.Add(page);
+            VisiblePopup.Add(page);
+            return Task.CompletedTask;
+        }
+
+        public Task RemoveAsync(PopupPage page)
+        {
+            VisiblePopup.Remove(page);
+            return Task.CompletedTask;
+        }
+    }
+}
